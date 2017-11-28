b08ab8d68f32005b3de7c41a55e61043e0a6e788
Aaron Wilder <element1028@gmail.com>
2017-11-28 11:35:55 -0800

    still working

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-
-
-$('#fb-icon').hover(
-    function() {
-        $( this ).addClass('animated tada')
-    },
-    function() {
-        $( this ).removeClass('animated tada')
-    });
+$(function() {
+    
+    $('#fb-icon').hover(
+        function() {
+            $( this ).addClass('animated tada')
+        },
+        function() {
+            $( this ).removeClass('animated tada')
+        });
+     
+        $('.brands__link').hover(
+            function() {
+                $( this ).addClass('animated pulse')
+            },
+            function() {
+                $( this ).removeClass('animated pulse')
+        });
+});
